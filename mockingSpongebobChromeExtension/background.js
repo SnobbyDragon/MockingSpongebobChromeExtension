0eb1af611592d936e5146b2f1198ca52a7c20d91
Ang <angela.li.2019@gmail.com>
2019-10-22 21:48:36 -0400

    mocking text font
    makes every other letter capitalized. currently alerts just to check it

--- mockingSpongebobChromeExtension/background.js
+++ mockingSpongebobChromeExtension/background.js
@@ -6,3 +6,15 @@ chrome.runtime.onInstalled.addListener(function() {
     contexts: ["selection"]
   });
 });
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  var output = [];
+  for (var i = 0; i < info.selectionText.length; i++) {
+    if (i%2 == 0) {
+      output.push(info.selectionText.charAt(i).toLowerCase());
+    } else {
+      output.push(info.selectionText.charAt(i).toUpperCase());
+    }
+  }
+  alert(output.join(""));
+});

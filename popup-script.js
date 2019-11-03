a20cf5097390a983dec75db333e2f10805f903cb
Ang <angela.li.2019@gmail.com>
2019-11-03 09:08:04 -0500

    prep text changes

--- popup-script.js
+++ popup-script.js
@@ -5,20 +5,27 @@ function drawImage(text) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   // ctx.beginPath();
   // ctx.rect(0, 0, canvas.width, canvas.height);
+
+  //draw original spongebob image
   const img = document.getElementById("mocking-spongebob");
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "white";
-  ctx.font = "48px Comic Sans MS";
-  ctx.fillText(text.current, 10, 50);
+
+  //draw text on image
+  var fontSize = "48px";
+  var xPos = 10, yPos = 50;
+  ctx.font = fontSize + " Comic Sans MS";
+  ctx.fillText(text.current, xPos, yPos);
   ctx.fillStyle = "black";
   ctx.lineWidth = 2;
-  ctx.strokeText(text.current, 10, 50);
+  ctx.strokeText(text.current, xPos, yPos);
 
+  //convert canvas to image
   var generatedMeme = document.createElement('img');
   generatedMeme.src = canvas.toDataURL()
   generatedMeme.style.width = canvas.width;
   generatedMeme.style.height = canvas.height;
-  document.body.appendChild(generatedMeme);
+  document.getElementById("formatText").appendChild(generatedMeme);
 }
 
 window.onload = function() {
